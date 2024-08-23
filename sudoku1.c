83c63b305ca3b300b6df5c03ce7f1b4b33510544
joaopedro135 <88681441+joaopedro135@users.noreply.github.com>
2024-08-23 18:22:08 -0300

    Add files via upload
    Matriz colorida

--- sudoku1.c
+++ sudoku1.c
@@ -1,16 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
+
+#define BLUE   "\x1b[34m"
+#define RESET  "\x1b[0m"
+
 int main () {
     for (int A = 0; A < 9; A++) {
-        printf ("____");
+        printf (BLUE "____" RESET);
     }
     for (int A = 0; A < 9; A++) {
-        printf ("\n|");
+        printf (BLUE "\n|" RESET);
         for (int B = 0; B < 9; B++) {
-            printf (" 0 |");
+            printf (" 0 ");
+            if ((B - 2) % 3 == 0 || B == 8) {
+                printf (BLUE "|" RESET);
+            } else {
+                printf ("|");
+            }
         }
-        printf ("\n|");
+        printf (BLUE "\n|" RESET);
         for (int B = 0; B < 9; B++) {
-            printf ("___|");
+            if ((A + 1) % 3 == 0 || A == 8) {
+                printf (BLUE "___" RESET);
+                if ((B + 1) % 3 == 0 || B == 8) {
+                    printf (BLUE "|" RESET);
+                } else {
+                    printf ("|");
+                }
+            } else {
+                printf ("___");
+                if ((B + 1) % 3 == 0 || B == 8) {
+                    printf (BLUE "|" RESET);
+                } else {
+                    printf ("|");
+                }
+            }
         }
     }
     return 0;
